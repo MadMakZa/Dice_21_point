2c834ca454ef3ecb23740acf2c6d49fbe4d2fdd8
MadMakZa <immakza@gmail.com>
2020-12-13 22:42:21 +0300

    class dice random number, add method setImage for Dice

--- app/src/main/java/com/example/dice_21_point/Dice.java
+++ app/src/main/java/com/example/dice_21_point/Dice.java
@@ -1,4 +1,47 @@
 package com.example.dice_21_point;
 
+/* *
+ * Игральный кубик, умеет генерировать рандомное число
+ * */
+
+import android.widget.ImageView;
+
 public class Dice {
+
+    //диапазон генерации чисел
+    private final int a = 1;
+    private final int b = 6;
+    private int random_number;
+
+    // генерация
+    private int generateRandomNumber(){
+        random_number = a + (int) (Math.random() * b);
+        return random_number;
+    }
+
+    //присвоить картинку в зависимости от выпавшего значения
+    public void generateDiceImage(){
+        switch (random_number) {
+            case 1:
+                MainActivity.getInstance().getImageDice().setImageResource(R.drawable.diceside_image1);
+                break;
+            case 2:
+                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube2);
+                break;
+            case 3:
+                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube3);
+                break;
+            case 4:
+                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube4);
+                break;
+            case 5:
+                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube5);
+                break;
+            case 6:
+                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube6);
+                break;
+    }
+
+
+
 }
--- app/src/main/java/com/example/dice_21_point/MainActivity.java
+++ app/src/main/java/com/example/dice_21_point/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.dice_21_point;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonBet;
+    private Button buttonRoll;
+    private Button buttonStop;
+    private ImageView imageDice;
+
+    public ImageView getImageDice() {
+        return imageDice;
+    }
+
+    public void setImageDice(ImageView imageDice) {
+        this.imageDice = imageDice;
+    }
+
+    //доступ к изменению переменных в этом классе
+    private static MainActivity instance;
+    public static MainActivity getInstance(){
+        return instance;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init(){
+        imageDice = findViewById(R.id.imageDiceOne);
+        buttonBet = findViewById(R.id.button_bet);
+        buttonRoll = findViewById(R.id.button_roll);
+        buttonStop = findViewById(R.id.button_stop);
     }
 }
