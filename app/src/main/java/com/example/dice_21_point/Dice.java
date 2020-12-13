e5629f72c06fea0c5700cb7e4c7c8deeab4cfa0b
MadMakZa <immakza@gmail.com>
2020-12-13 22:50:52 +0300

    class dice random number, add method setImage for Dice, add images

--- app/src/main/java/com/example/dice_21_point/Dice.java
+++ app/src/main/java/com/example/dice_21_point/Dice.java
@@ -20,26 +20,27 @@ public class Dice {
     }
 
     //присвоить картинку в зависимости от выпавшего значения
-    public void generateDiceImage(){
+    public void generateDiceImage() {
         switch (random_number) {
             case 1:
-                MainActivity.getInstance().getImageDice().setImageResource(R.drawable.diceside_image1);
+                MainActivity.getInstance().getImageDice().setImageResource(R.drawable.dice_side1);
                 break;
             case 2:
-                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube2);
+                MainActivity.getInstance().getImageDice().setImageResource(R.drawable.dice_side2);
                 break;
             case 3:
-                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube3);
+                MainActivity.getInstance().getImageDice().setImageResource(R.drawable.dice_side3);
                 break;
             case 4:
-                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube4);
+                MainActivity.getInstance().getImageDice().setImageResource(R.drawable.dice_side4);
                 break;
             case 5:
-                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube5);
+                MainActivity.getInstance().getImageDice().setImageResource(R.drawable.dice_side5);
                 break;
             case 6:
-                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube6);
+                MainActivity.getInstance().getImageDice().setImageResource(R.drawable.dice_side6);
                 break;
+        }
     }
 
 
